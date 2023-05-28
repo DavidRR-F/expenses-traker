aced073c0eba929283e05b02e618d00e693b71c8
DavidRR-F <david.rosefranklin96@gmail.com>
2023-05-28 09:09:16 -0400

    expense filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import Card from "../Shared/Card";
+import ExpensesFilter from "../newExpense/ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
 const Expenses = ({expenses}) => {
+    const [filteredYear, setFilteredYear] = useState(new Date().getFullYear());
+    const filterChangedHandler = selectedYear => {
+        setFilteredYear(selectedYear)
+    };
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangedHandler}/>
             {expenses.map((expense, index) => (
                 <ExpenseItem
                     title={expense.title}
--- src/components/newExpense/ExpensesFilter.js
+++ src/components/newExpense/ExpensesFilter.js
@@ -1,7 +1,22 @@
 import './ExpensesFilter.css'
 
-const ExpensesFilter = () => {
-    return ( <h1>Filter</h1> );
+const ExpensesFilter = (props) => {
+    const year = new Date().getFullYear();
+    const dropDownChangeHandler = (event) => {
+        props.onChangeFilter(event.target.value);
+    };
+    return ( 
+        <div className='expenses-filter'>
+            <div className='expenses-filter__control'>
+                <label>Filter by year</label>
+                <select value={props.selected} onChange={dropDownChangeHandler}>
+                    <option value={year}>{year}</option>
+                    <option value={year-1}>{year-1}</option>
+                    <option value={year-2}>{year-2}</option>
+                </select>
+            </div>
+        </div> 
+    );
 }
  
 export default ExpensesFilter;
